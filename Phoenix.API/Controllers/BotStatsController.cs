bf4448a5917caa31d5b59231accf70fbaaa56dcc
Callum Cross <callum.cross@immixprotect.com>
2024-12-23 20:46:20 +0000

    Added: BotStats Controller

---
+++ Phoenix.API/Controllers/BotStatsController.cs
@@ -0,0 +1,28 @@
+﻿using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Phoenix.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BotStatsController : ControllerBase
+    {
+        private readonly ILogger<BotStatsController> _logger;
+
+        public BotStatsController(ILogger<BotStatsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<bool> Get()
+        {
+            FirestoreDb firestoreDb = await FirestoreDb.CreateAsync("phoenix-430818");
+
+            CollectionReference collection = firestoreDb.Collection("users");
+            DocumentReference document = await collection.AddAsync(new { Name = new { First = "Ada", Last = "Lovelace" }, Born = 1815 });
+
+            return true;
+        }
+    }
+}
