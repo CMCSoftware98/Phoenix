1cb489af06771bd06561e93ef2ff2f7a9976313b
Callum Cross <callum.cross@immixprotect.com>
2024-12-23 21:15:41 +0000

    Added: Firestore DB

--- Phoenix.API/Controllers/BotStatsController.cs
+++ Phoenix.API/Controllers/BotStatsController.cs
@@ -8,18 +8,18 @@ namespace Phoenix.API.Controllers
     public class BotStatsController : ControllerBase
     {
         private readonly ILogger<BotStatsController> _logger;
+        private readonly FirestoreDb _dbContext;
 
-        public BotStatsController(ILogger<BotStatsController> logger)
+        public BotStatsController(ILogger<BotStatsController> logger, FirestoreDb firestoreDb)
         {
             _logger = logger;
+            _dbContext = firestoreDb;
         }
 
         [HttpGet]
         public async Task<bool> Get()
         {
-            FirestoreDb firestoreDb = await FirestoreDb.CreateAsync("phoenix-430818");
-
-            CollectionReference collection = firestoreDb.Collection("users");
+            CollectionReference collection = _dbContext.Collection("users");
             DocumentReference document = await collection.AddAsync(new { Name = new { First = "Ada", Last = "Lovelace" }, Born = 1815 });
 
             return true;
--- Phoenix.API/Program.cs
+++ Phoenix.API/Program.cs
@@ -1,3 +1,5 @@
+using Google.Cloud.Firestore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddFirestoreDb(x =>
+{
+    x.ProjectId = "phoenix-430818";
+    x.DatabaseId = "phoenix-db";
+    x.CredentialsPath = Directory.GetCurrentDirectory() + "\\auth.json";
+});
 
 var app = builder.Build();
 
