5c6f3a10f47c337ece4df56205ed50fe80945b05
Callum Cross <callum.cross@immixprotect.com>
2024-12-23 23:53:39 +0000

    updated BotStatsService

--- Phoenix.API/Service/BotStatsService.cs
+++ Phoenix.API/Service/BotStatsService.cs
@@ -6,7 +6,5 @@ namespace Phoenix.API.Service
     {
         private readonly ILogger<BotStatsService> _logger = logger;
         private readonly FirestoreDb _firestoreDb = firestoreDb;
-
-        public async  
     }
 }
